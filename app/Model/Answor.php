77f16baf42629b658e958164985cbedb1461527b
thatateesta <thatateesta@gmail.com>
2019-05-01 10:31:45 +0530

    releationship connected for databases

--- app/Model/Answor.php
+++ app/Model/Answor.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answor extends Model
 {
-    //
+   public function quations()
+   {
+       return $this->belongsTo(quation::class);
+   }
+
+   public function users()
+   {
+       return $this->belongsTo(USER::class);
+    
+   }
+
+   public function likes()
+   {
+       return $this->hasMany(Like::class);
+   }
 }
--- app/Model/quation.php
+++ app/Model/quation.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class quation extends Model
 {
-    //
+        
+    public function users()
+        {
+        
+        return $this->belongsTo(User::class);
+            
+        }
+
+    public function answors()
+        {
+
+        return $this->hasMany(Answor::class);
+
+        }
+    public function categori()
+    {
+        return $this->belongsTo(Catagory::class);
+    }
+
+
 }
